e35116796cd64e69d1cb2f3db1210022f32a4280
xVennce <145119672+xVennce@users.noreply.github.com>
2026-04-20 17:55:35 +0100

    interactables ready for second level

--- UnityProj/FYP/Assets/Scripts/Interactables/Herb.cs
+++ UnityProj/FYP/Assets/Scripts/Interactables/Herb.cs
@@ -7,11 +7,33 @@ public class Herb : InteractionBase {
     protected override void Update() {
     }
     protected override void HandleInteract() {
+        //Level check
+        LevelCheck();
+    }
+    private void IncrementCheck() {
         if (isInteractable) {
             Debug.Log("Interacted with herb");
             isInteractable = false;
             inventory.IncrementHerb(1);
             DeleteSelf(destroyDelay);
+        }
+    }
+    private void LevelCheck() {
+        switch (GlobalVariables.currentLevel) {
+            case GlobalVariables.CurrentLevel.Level_One:
+                IncrementCheck();
+                break;
+            case GlobalVariables.CurrentLevel.Level_Two:
+                if (inventory.hasShears == false) {
+                    player.ShowDialogueForXTime("I need something to cut this herb...");
+                }
+                else {
+                    IncrementCheck();
+                }
+                break;
+            case GlobalVariables.CurrentLevel.Level_Three:
+                //level check for level 3
+                break;
         }
     }
 }
--- UnityProj/FYP/Assets/Scripts/Interactables/Wood.cs
+++ UnityProj/FYP/Assets/Scripts/Interactables/Wood.cs
@@ -7,11 +7,33 @@ public class Wood : InteractionBase {
     protected override void Update() {
     }
     protected override void HandleInteract() {
+        //Level check
+        LevelCheck();
+    }
+    private void IncrementCheck() {
         if (isInteractable) {
             Debug.Log("Interacted with wood");
             isInteractable = false;
             inventory.IncrementWood(1);
             DeleteSelf(destroyDelay);
+        }
+    }
+    private void LevelCheck() {
+        switch (GlobalVariables.currentLevel) {
+            case GlobalVariables.CurrentLevel.Level_One:
+                IncrementCheck();
+                break;
+            case GlobalVariables.CurrentLevel.Level_Two:
+                if (inventory.hasAxe == false) {
+                    player.ShowDialogueForXTime("I need something to chop the tree...");
+                }
+                else {
+                    IncrementCheck();
+                }
+                break;
+            case GlobalVariables.CurrentLevel.Level_Three:
+                //level check for level 3
+                break;
         }
     }
 }
